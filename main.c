2f9a54f86212c0bf69c93e0449dad8256d8d2e1c
Matt Foster <matt.p.foster@gmail.com>
2011-09-21 13:20:41 +0100

    Add some documentation and tidy comments.
    Also, make count unsigned.

--- main.c
+++ main.c
@@ -1,9 +1,30 @@
 /*
-    File:       main.c
-    Version:    1.0.0
-    Date:       May. 12, 2006
 
     TinyAlarm - ATTiny45V based alarm
+
+    TinyAlarm is an very simple interrupt based alarm system.
+
+    When first powered on, the alarm beeps for a duration defined by the
+    constant STARTUP_DELAY, indicating that the alarm will soon be armed.
+    During this time, it will not react to external stimuli.
+
+    After this initial delay, the system sleeps, waiting for a pin-change
+    interrupt (on pin 5 by default). When a change is detected on this pin, the
+    alarm enters an initial alert phase during which it beeps to indicate it
+    has been activated. An indicator LED is also activated. The length of this
+    phase is defined by the constant ALERT_1_DURATION.
+
+    After the second delay, the alarm becomes fully active and the alarm siren
+    goes off for a period defined by ALERT_2_DURATION.
+
+    Notes:
+     * The duration constants have units of 0.5s, or the watchdog timeout
+       period if modified.
+     * The count variable is an unsigned integer. This means the maximum
+       duration is 255/2 seconds. Count could be made 'long' if necessary.
+     * Only one pin-change interrupt is active by default. A second could be
+       added by modifying the *_pcie functions and adding an ISR.
+     * I'm not sure I've implemented the sleep functionality correctly!
 
 */
 
@@ -14,6 +35,10 @@
 #include <inttypes.h>
 #include <avr/sleep.h>
 
+#define STARTUP_DELAY 30
+#define ALERT_1_DURATION 30
+#define ALERT_2_DURATION 30
+
 enum states {
     STARTUP,
     ARMED,
@@ -23,22 +48,17 @@ enum states {
 };
 
 volatile enum states state = STARTUP;
-volatile int count = 0;
+volatile unsigned int count = 0;
 volatile int toggled = 0;
 
-
-#define STARTUP_DELAY 30
-#define ALERT_1_DURATION 30
-#define ALERT_2_DURATION 30
-
-// Enable hardware interrupt.
+/*  Enable hardware interrupt. */
 void enable_pcie()
 {
     GIMSK |= 1<<PCIE;
     PCMSK |= 1<<PCINT0;
 }
 
-// Disable the external interrupt.
+/*  Disable the external interrupt. */
 void disable_pcie()
 {
     GIMSK &= ~(1<<PCIE);
@@ -49,25 +69,25 @@ void act()
 {
     count++;
 
-    // Startup state. Beep for a while.
+    /*  Startup state. Beep for a while. */
     switch(state) {
         case  STARTUP:
-            // When we get here, transition to the ARMED state and disable the clock.
+            /*  When we get here, transition to the ARMED state and disable the clock. */
             if (count == STARTUP_DELAY) {
 
-                // Disable WDT interrupt
+                /*  Disable WDT interrupt */
                 WDTCR &= ~_BV(WDIE);
 
                 enable_pcie();
 
-                // Alarm off.
+                /*  Alarm off. */
                 PORTB &= ~_BV(PB4);
 
-                // Reset count for the next state which needs it.
+                /*  Reset count for the next state which needs it. */
                 count = 0;
                 state = ARMED;
             }
-            // Otherwise we're still in STARTUP, so toggle the alarm.
+            /*  Otherwise we're still in STARTUP, so toggle the alarm. */
             else {
                 PORTB ^= _BV(PB4);
             }
@@ -78,67 +98,67 @@ void act()
             }
             break;
         case ALERT_1:
-                // Enable the indicator LED - stays on forever now :)
+                /*  Enable the indicator LED - stays on forever now :) */
                 PORTB |= _BV(PB3);
 
-                // Beep for a while. Then Transition to the next state
-                // Leave the clock running.
+                /*  Beep for a while. Then Transition to the next state */
+                /*  Leave the clock running. */
                 if (count == ALERT_1_DURATION) {
                     state = ALERT_2;
-                    // Enable the alarm:
+                    /*  Enable the alarm: */
                     PORTB |= _BV(PB4);
 
-                    // Reset count for the next state which needs it.
+                    /*  Reset count for the next state which needs it. */
                     count = 0;
                 }
-                // Otherwise we're still in ALERT_1, so blink.
+                /*  Otherwise we're still in ALERT_1, so blink. */
                 else {
                     PORTB ^= _BV(PB4);
                 }
                 break;
-                // In ALERT_2 we just make a noise for a while
+                /*  In ALERT_2 we just make a noise for a while */
         case ALERT_2:
-                // The LED was swtiched on at the end of the last state. Leave it for now.
+                /*  The LED was swtiched on at the end of the last state. Leave it for now. */
                 if (count == ALERT_2_DURATION) {
-                    // Now disable the clock, and transition to the final state
+                    /*  Now disable the clock, and transition to the final state */
                     WDTCR &= ~_BV(WDIE);
                     PORTB &= ~_BV(PB4);
                     state = ALERT_3;
 
-                    // Reset count for the next state which needs it.
+                    /*  Reset count for the next state which needs it. */
                     count = 0;
                 }
                 break;
         case ALERT_3:
-                // Nothing specific happens here. The LED should be on.
+                /*  Nothing specific happens here. The LED should be on. */
                 break;
     }
 }
 
-// WDT Timeout Interrupt
+/*  WDT Timeout Interrupt */
 ISR(WDT_vect) {
     act();
 }
 
-// External interrupt 0
+/*  External interrupt 0 */
 ISR(PCINT0_vect) {
     disable_pcie();
 
     toggled = 1;
-    // re-enable the clock
+    /*  re-enable the clock */
     WDTCR |= _BV(WDIE);
 }
 
 void init (){
     /* PB4 is digital output */
-    DDRB = _BV (PB4);
+    DDRB |= _BV (PB4);
     /* PB3 is digital output */
     DDRB |= _BV (PB3);
 
-    // Set up the watchdog timer - ~0.5s timeout
+    /* Set up the watchdog timer - ~0.5s timeout */
     WDTCR = (1<<WDP2) | (1<<WDP0) | (1<<WDIE);
 
-    // Enable pin change inerrupts
+    /* Enable pin change inerrupts */
     enable_pcie();
 
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
